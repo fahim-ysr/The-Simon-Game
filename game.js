39a115cdcac0d55aa4e9f40112e5c15a82ae4318
Fahim <fyasir@mun.ca>
2024-07-13 03:09:38 -0230

    Step 4: Checking Which Button is Pressed

--- game.js
+++ game.js
@@ -1,45 +1,44 @@
-"use strict";
-/* 
-var level = 0;
-
-$(document).keypress(function (event) {
-  //   console.log(event.key);
-  if (event.key == "a" || event.key == "A") {
-    console.log("Pressed the right button!");
-    $("h1").text("Level " + level);
-    level++;
-  }
-});
-*/
+// "use strict";
 
 const buttonColors = ["red", "green", "blue", "yellow"];
 var randomChosenColor;
 const gamePattern = [];
+const userClickedPattern = [];
 
 // *Step 2: Create A New Pattern that chooses a random color from the array buttonColors
 
 // This selects a random color from array buttonColors and adds to the array gamePattern
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  gamePattern.push(buttonColors[randomNumber]);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
   console.log(gamePattern);
 
   // *Step 3: Showing the sequence to user with animations and sounds
   // Press the specific button (Keydown action)
-  $(`.${buttonColors[randomNumber]}`).addClass("pressed");
+  $(`.${randomChosenColor}`).addClass("pressed");
 
   // Removes the press from the button after 100ms (Keyup action)
   setTimeout(function () {
-    $(`.${buttonColors[randomNumber]}`).removeClass("pressed");
+    $(`.${randomChosenColor}`).removeClass("pressed");
   }, 100);
 
-  function play() {
-    var audio = new Audio(`sounds/${buttonColors[randomNumber]}.mp3`);
-    // console.log(audio);  //For checking if audio file is correct
-    audio.play();
-  }
-  play();
+  /* 
+  var audio = new Audio(`sounds/${randomChosenColor}.mp3`);
+  // console.log(audio);  //For checking if audio file is correct
+  audio.play();
+  */
 }
 
 // Used for verification purposes
 nextSequence();
+
+// *Step 4: Checking Which Button is Pressed
+
+// Creating the handler function to detect which colors are chosen
+$(".btn").on("click", function () {
+  // alert("Clicked!"); // Just to check if button clicks are detected
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
+});
